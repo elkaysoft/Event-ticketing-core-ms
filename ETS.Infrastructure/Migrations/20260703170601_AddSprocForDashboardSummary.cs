f72b7ff7b3f43cc36f374cce810e5e347bcd5ee8
Olamilekan <o.kareem@ucard.store>
2026-07-04 07:50:45 +0100

    added stored procedure for generating dashboard summary and analytics

---
+++ ETS.Infrastructure/Migrations/20260703170601_AddSprocForDashboardSummary.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ETS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSprocForDashboardSummary : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE OR ALTER PROCEDURE [dbo].[sproc_GetDashboardSummary]
+	        @StartDate DATETIME,
+	        @EndDate DATETIME
+        AS
+        BEGIN
+	        SET NOCOUNT ON;
+            DECLARE @TotalTickets INT;
+            DECLARE @ActiveEvents INT;
+
+            -- Get total tickets from Events
+            SELECT @TotalTickets = SUM(ev.ItemCount)
+            FROM [Events] ev
+            WHERE ev.CreatedAt >= @StartDate
+            AND ev.CreatedAt <= @EndDate;
+
+            SELECT @ActiveEvents = COUNT(p.Id)
+            FROM [Events] p
+            WHERE p.EventDate >= @StartDate
+            AND p.EventDate <= @EndDate;
+
+	         -- Daily breakdown CTE (reused for summary)
+            WITH TicketRevenue AS (
+                SELECT
+                    CAST(oi.CreatedAt AS DATE)              AS TransactionDate,
+                    SUM(oi.UnitPrice * oi.Unit)             AS DailyRevenue,
+                    SUM(oi.Unit)                            AS DailyTicketsSold
+                FROM OrderItem oi
+                WHERE oi.PaymentStatus = 'Completed'
+                  AND oi.CreatedAt >= @StartDate
+                  AND oi.CreatedAt <= @EndDate
+                GROUP BY CAST(oi.CreatedAt AS DATE)
+            )
+
+            SELECT 
+                COALESCE(SUM(tr.DailyRevenue), 0) AS TotalRevenue,
+                COALESCE(SUM(tr.DailyTicketsSold), 0) AS TotalTicketsSold,
+                COALESCE(@TotalTickets, 0) AS TotalTickets,
+                COALESCE(@ActiveEvents, 0) AS ActiveEvents
+                FROM TicketRevenue tr;
+
+            END;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP PROCEDURE [dbo].[sproc_GetDashboardSummary]");
+        }
+    }
+}
---
+++ ETS.Infrastructure/Migrations/20260703175058_AddSprocForDashboardAnalytics.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ETS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSprocForDashboardAnalytics : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE OR ALTER PROCEDURE [dbo].[sproc_GetDashboardAnalytics]
+	@StartDate DATETIME,
+	@EndDate DATETIME
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	SELECT
+    DATENAME(MONTH, oi.CreatedAt) + ', ' + CAST(YEAR(oi.CreatedAt) AS VARCHAR(4)) AS TransactionMonth,
+    SUM(oi.Unit) AS TotalTicketsSold
+FROM OrderItem oi
+WHERE oi.PaymentStatus = 'Completed'
+  AND oi.CreatedAt >= @StartDate
+  AND oi.CreatedAt <= @EndDate
+GROUP BY
+    YEAR(oi.CreatedAt),
+    MONTH(oi.CreatedAt),
+    DATENAME(MONTH, oi.CreatedAt)
+ORDER BY
+    YEAR(oi.CreatedAt),
+    MONTH(oi.CreatedAt);
+
+END
+GO
+
+");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP PROCEDURE IF EXISTS [dbo].[sproc_GetDashboardAnalytics]");
+        }
+    }
+}
